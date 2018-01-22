82482dc08f0e3d419fdf605962802b1756381bae
irina <mininairn@yandex.ru>
2018-01-22 16:30:26 +0300

    создание консольки сумма

---
+++ Calculator/CalcConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //calc.exe sum 1 2
+            var oper = args[0];
+            double x = Convert.ToDouble(args[1]);
+            double y = Convert.ToDouble(args[2]);
+            double rez = 0.0; 
+            
+            switch (oper)
+            {
+                case "sum": rez = x + y; break;
+                    //Console.WriteLine($"sum {x} {y} = {x + y}");
+            //        default;
+            }
+            Console.WriteLine(rez.ToString());
+            Console.ReadKey();
+           }
+    }
+}
