423de016c86c3c94f48604d90640ca94b11e009e
irina <mininairn@yandex.ru>
2018-01-23 14:28:58 +0300

    тесты

---
+++ Calculator/CalcConsole/Calc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcConsole
+{
+    public class Calc
+    {
+        public double Sum(double x, double y)
+        {
+            return x+y;
+        }
+        public double Sub(double x, double y)
+        {
+            return x-y;
+        }
+        public double Mul(double x, double y)
+        {
+            return x * y;
+        }
+        public double Div(double x, double y)
+        {
+            if (y != 0)
+                return x / y;
+            else
+                return Double.NaN;
+        }
+        public double Pow(double x, double y)
+        {
+            return Math.Pow(x , y);
+        }
+        public double Sqrt(double x)
+        {
+            return Math.Sqrt(x);
+        }
+    }
+}
---
+++ Calculator/TestCalcConsole/UnitTest1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalcConsole;
+
+namespace TestCalcConsole
+{
+    [TestClass]
+    public class CalcTest
+    {
+        [TestMethod]
+        public void TestSum()
+        {
+            //arrange
+            var x = 10;
+            var y = 1;
+            var calc = new Calc();
+
+            //act
+            var rezult = calc.Sum(x , y);
+
+            //assert
+            Assert.AreEqual(rezult, 11);
+
+        }
+        
+        [TestMethod]
+        public void TestSub()
+        {
+            //arrange
+            var x = 10;
+            var y = 1;
+            var calc = new Calc();
+
+            //act
+            var rezult = calc.Sub(x, y);
+
+            //assert
+            Assert.AreEqual(rezult, 9);
+
+        }
+        [TestMethod]
+        public void TestMul()
+        {
+            //arrange
+            var x = 10;
+            var y = 2;
+            var calc = new Calc();
+
+            //act
+            var rezult = calc.Mul(x, y);
+
+            //assert
+            Assert.AreEqual(rezult, 20);
+
+        }
+        [TestMethod]
+        public void TestDiv()
+        {
+            //arrange
+            var x = 10;
+            var y = 2;
+            var calc = new Calc();
+
+            //act
+            var rezult = calc.Div(x, y);
+
+            //assert
+            Assert.AreEqual(rezult, 5);
+
+        }
+        [TestMethod]
+        public void TestDivZero()
+        {
+            //arrange
+            var x = 10.0;
+            var y = 0.0;
+            var calc = new Calc();
+
+            //act
+            var rezult = calc.Div(x, y);
+
+            //assert
+            Assert.AreEqual(rezult, Double.NaN);
+
+        }
+        [TestMethod]
+        public void TestPow()
+        {
+            //arrange
+            var x = 2;
+            var y = 3;
+            var calc = new Calc();
+
+            //act
+            var rezult = calc.Pow(x, y);
+
+            //assert
+            Assert.AreEqual(rezult, 8);
+
+        }
+        [TestMethod]
+        public void TestSqrt()
+        {
+            //arrange
+            var x = 16;
+           
+            var calc = new Calc();
+
+            //act
+            var rezult = calc.Sqrt(x);
+
+            //assert
+            Assert.AreEqual(rezult, 4);
+
+        }
+    }
+}
